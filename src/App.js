fa4dd6bf6e3de7de6ba10e84dc1973bead5b332b
Jake Shams <jshams62@gmail.com>
2019-04-08 12:14:42 -0700

    button clicks refine items displayed

--- src/App.js
+++ src/App.js
@@ -8,13 +8,21 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      currentCategory: null
+      currentCategory: 'Sports'
     }
   }
 
   setCategory(cat) {
     console.log(cat)
     this.setState({ currentCategory: cat })
+    // this.state.currentCategory = cat
+  }
+
+  filterByCat(item) {
+    // this.currentCategory = 'Toys'
+    console.log(this.state.currentCategory)
+    if (this.state.currentCategory == null) return true
+    return item.category === this.state.currentCategory
   }
 
   render() {
@@ -29,10 +37,16 @@ class App extends Component {
       )
     })
 
+    // if (currcat === null) return true
+    // return currcat === item.cat
 
-    const items = inventory.map(({ name, description, price }, i) => {
+    // .filter((item) => { !currentCategory ? pass : item.category === currentCategory)
+    // .filter((item) => {item.category == "Toys})
+
+    const items = inventory.filter((item) => this.filterByCat(item)).map(({ name, description, price, category }, i) => {
       return (<Product
         key={i}
+        cat={category}
         title={name}
         desc={description}
         price={price}
@@ -42,6 +56,7 @@ class App extends Component {
     return (
       <div className="App" >
         <h1>Show products here</h1>
+        {cats}
 
         <ul className="inventory">
           {items}
--- src/Product.js
+++ src/Product.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Product = ({ title, desc, price }) => {
+const Product = ({ title, desc, price, cat }) => {
     return (
         <div className="item-card">
             <h1 className="title">{title}</h1>
+            <p className="cat">{cat}</p>
             <p className="desc">{desc}</p>
             <p className="price">${price}</p>
         </div>
